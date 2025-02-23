28cb79da2c7988af1dc08aedc391e2b803345321
car1nhanha <lucascarinhanha4@gmail.com>
2025-02-23 12:36:42 -0400

    atualizado scripts no package.json para usar npx e removidos logs de configuração do banco de dados

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,10 +8,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   console.log({
-    DATABASE_HOST: process.env.DATABASE_HOST,
-    DATABASE_PORT: process.env.DATABASE_PORT,
-    DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
     DATABASE_NAME: process.env.DATABASE_NAME,
   });
 
